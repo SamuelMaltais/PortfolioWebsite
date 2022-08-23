858813191971111f3d8a1fdc021244549d6e1aac
SamuelMaltais <leumasmontreal@hotmail.com>
2022-08-23 15:23:20 -0400

    Added project cards and mcgill intership

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 class AboutMe extends Component {
-  constructor(props) {
-    super(props);
-  }
   state = {};
   render() {
     return <h1>Hello</h1>;
